671f94e206035609e59747f1ad97b628992136fa
Abdumajidov-Bobojon <abdumajidovbobojon9@gmail.com>
2022-01-29 10:43:05 +0500

    muhit sozlandi

---
+++ data/script.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1,
+        title: "Non",
+        price: 3000 
+    },
+    {
+        id: 2,
+        title: "Cola",
+        price: 10000 
+    },
+    {
+        id: 3,
+        title: "Olma",
+        price: 15000 
+    },
+    {
+        id: 4,
+        title: "Uzum",
+        price: 7000 
+    },
+
+]
